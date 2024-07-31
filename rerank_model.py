9288e4c909be3d23a76ef73b153ff77e5319a71b
dding333 <113060541+dding333@users.noreply.github.com>
2024-07-30 21:07:42 -0400

    Update rerank_model.py

--- rerank_model.py
+++ rerank_model.py
@@ -13,7 +13,7 @@ DEVICE_ID = "0"
 CUDA_DEVICE = f"{DEVICE}:{DEVICE_ID}" if DEVICE_ID else DEVICE
 
 
-# 释放gpu上没有用到的显存以及显存碎片
+# release the storage of GPU
 def torch_gc():
     if torch.cuda.is_available():
         with torch.cuda.device(CUDA_DEVICE):
@@ -21,7 +21,7 @@ def torch_gc():
             torch.cuda.ipc_collect()
 
 
-# 加载rerank模型
+# load rerank model
 class reRankLLM(object):
     def __init__(self, model_path, max_length = 512):
         self.tokenizer = AutoTokenizer.from_pretrained(model_path)
@@ -32,7 +32,7 @@ class reRankLLM(object):
         # self.model.to(torch.device("cuda:1"))
         self.max_length = max_length
 
-    # 输入文档对，返回每一对(query, doc)的相关得分，并从大到小排序
+    # use (query,doc) as input，get scores and rerank
     def predict(self, query, docs):
         pairs = [(query, doc.page_content) for doc in docs]
         # inputs = self.tokenizer(pairs, padding=True, truncation=True, return_tensors='pt', max_length=self.max_length).to("cuda:1")
